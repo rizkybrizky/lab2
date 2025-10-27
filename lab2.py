13045ddb96a0b224f40e6426b2666b2c6782ab79
Hariez <muhammadhariezzz03.24@ichat.sp.edu.sg>
2025-10-27 15:41:05 +0800

    bmi calculation

--- lab2.py
+++ lab2.py
@@ -1,1 +1,14 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def calculate_bmi(height, weight):
+ print("Height = " + str(height))
+ print("Weight = " + str(weight))
+ bmi=weight/(height*height)
+ print(str(bmi))
+ if(bmi<18.5):
+    print("Under Weight")
+ elif(bmi>25.0):
+   print("Over Weight")
+ else:
+   print("Normal Weight")
+ 
+
+calculate_bmi(weight=57, height=1.73)
